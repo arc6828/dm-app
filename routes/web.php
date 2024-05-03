4f73869d119550b5352eb7fbbc1990947349103d
arc6828 <chavalit.kow@gmail.com>
2024-05-03 17:25:43 +0700

    Theme

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+    // return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -29,3 +30,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/about', function(){
+    return view("about");
+});
